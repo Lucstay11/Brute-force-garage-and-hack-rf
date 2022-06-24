cc44ddac16e716ea6b20a75e141d5042c68714d3
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2022-06-24 04:56:57 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "INSTALLING START ..." | toilet -f term -F border
+sudo apt install toilet -y
+sudo apt install lolcat -y
+sudo apt install whiptail -y
+sudo apt install php -y
+sudo apt install rtl-sdr -y
+sudo apt-get install rtl-433 -y
+echo "INSTALLING FINISH!! --> bash rf.sh" | toilet -f term -F border
