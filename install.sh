72c73a83118ef7ad8529a7fec8a3b6c770fcf567
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2022-06-24 23:59:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ cd fm_transmitter
 make
 cd ..
 mv fm_transmitter .fm_transmitter
-#sudo apt-get install make build-essential
-#sudo apt-get install libraspberrypi-dev
-#sudo apt install toilet -y
-#sudo apt install lolcat -y
-#sudo apt install whiptail -y
-#sudo apt install php -y
-#sudo apt install rtl-sdr -y
-#sudo apt-get install rtl-433 -y
+sudo apt-get install make build-essential
+sudo apt-get install libraspberrypi-dev
+sudo apt install toilet -y
+sudo apt install lolcat -y
+sudo apt install whiptail -y
+sudo apt install php -y
+sudo apt install rtl-sdr -y
+sudo apt-get install rtl-433 -y
 echo "INSTALLING FINISH!! --> bash rf.sh"
