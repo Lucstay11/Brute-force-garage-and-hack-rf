3d47c0a9660dcf709b6da2009bd338ae6cdbfff2
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2022-06-24 22:22:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-echo "INSTALLING START ..." | toilet -f term -F border
+echo "INSTALLING START ..."
+sudo apt-get update
+sudo apt-get install make build-essential
+sudo apt-get install libraspberrypi-dev
 sudo apt install toilet -y
 sudo apt install lolcat -y
 sudo apt install whiptail -y
 sudo apt install php -y
 sudo apt install rtl-sdr -y
 sudo apt-get install rtl-433 -y
-echo "INSTALLING FINISH!! --> bash rf.sh" | toilet -f term -F border
+echo "INSTALLING FINISH!! --> bash rf.sh"
