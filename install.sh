060e1c0594f8dde67ba9534d76b4962428f67107
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2023-02-19 17:05:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ cd fm_transmitter
 make
 cd ..
 mv fm_transmitter .fm_transmitter
-sudo apt-get install make build-essential
-sudo apt-get install libraspberrypi-dev
+sudo apt-get install make build-essential -y
+sudo apt-get install libraspberrypi-dev -y
 sudo apt install toilet -y
 sudo apt install lolcat -y
 sudo apt install whiptail -y
