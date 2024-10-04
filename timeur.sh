95590a25c462147f4e2429face0ba567aedcbf61
Lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2024-10-04 16:37:48 +0200

    Update timeur.sh

--- timeur.sh
+++ timeur.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
- hour=0
- #min
- sec=0
-        while [ $hour -ge 0 ]; do
-                 while [ $1 -ge 0 ]; do
-                         while [ $sec -ge 0 ]; do
-                                 echo -ne "$hour:$1:$sec\033[0K\r"
-                                 let "sec=sec-1"
-                                 sleep 1
-                         done
-                         sec=59
-                         let "min=min-1"
-                 done
-                 min=59
-                 let "hour=hour-1"
-         done
+hour=0 
+min=$1 
+sec=0
+
+# Timer
+while [ $hour -ge 0 ]; do
+    while [ $min -ge 0 ]; do
+        while [ $sec -ge 0 ]; do
+            printf "%02d:%02d:%02d\033[0K\r" $hour $min $sec
+            let "sec=sec-1"
+            sleep 1
+        done
+        sec=59
+        let "min=min-1"
+    done
+    min=59
+    let "hour=hour-1"
+done
+
+echo "Finish!"
+
